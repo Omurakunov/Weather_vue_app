dae03b55bcf4c99ea1669d3edd1f0fd47baa5cbc
Omurakunov Abdulaziz <77117647+Omurakunov@users.noreply.github.com>
2021-11-22 14:23:38 +0600

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ new Vue({
   },
   methods: {
     getRequest(city){
-      this.check = true
+      
       fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=163796f7f9173e8d72c4febb054e60ba`)
         .then(res =>{
           return(res.json()) 
@@ -17,7 +17,7 @@ new Vue({
     },
     setResults(results){
       this.object = results
-      
+      this.check = true
       console.log(check)
     }
 
